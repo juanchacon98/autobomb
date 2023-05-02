997b695c6cda11fc7b70eff0380d369e6990d066
juanchacon98 <95106627+juanchacon98@users.noreply.github.com>
2023-05-02 10:40:28 -0400

    Add files via upload

--- home/site/wwwroot/database.js
+++ home/site/wwwroot/database.js
@@ -1,3 +1,31 @@
+<<<<<<< HEAD
+const mysql = require('mysql2');
+const fs = require('fs');
+
+const connection = mysql.createConnection({
+  host: 'autobombapp.mysql.database.azure.com',
+  user: 'autobomb',
+  password: '{Ricardo07=}',
+  database: '{agua}',
+  port: 3306,
+  ssl: { ca: fs.readFileSync('{./certs/DigiCertGlobalRootCA.crt.pem}') },
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+const pool = connection.promise();
+
+module.exports = {
+  connection: connection,
+  poolPromise: pool,
+};
+=======
 const mysql = require('mysql2');
 const fs = require('fs');
 
@@ -24,3 +52,4 @@ module.exports = {
   connection: connection,
   poolPromise: pool,
 };
+>>>>>>> 0a927d24906c455b5229cb212fd080f809ec8b28
